33d1e407537d3bd8ff80f3d4c754bceda74cdbc4
fteyguhdij <hsjcallpaige434@hotmail.com>
2024-05-11 21:00:47 +0700

    hello

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
-function combinationSum3(k, n) {
-  const result = [];
-  backtrack([], 1, k, n);
-  return result;
-  function backtrack(combination, start, k, n) {
-    if (n === 0 && k === 0) {
-      result.push([...combination]);
-      return;
-    }
-    if (n < 0 || k === 0) return;
-    for (let i = start; i <= 9; i++) {
-      combination.push(i);
-      backtrack(combination, i + 1, k - 1, n - i);
-      combination.pop();
+function combinationSum4(nums, target) {
+  const dp = new Array(target + 1).fill(0);
+  dp[0] = 1;
+  for (let i = 1; i <= target; i++) {
+    for (const num of nums) {
+      if (i >= num) {
+        dp[i] += dp[i - num];
+      }
     }
   }
+  return dp[target];
 }
